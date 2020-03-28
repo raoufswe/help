9f7a8eff01d45e7fc35dd721368218f511da1156
raoufswe <abdulraoufswehli58@gmail.com>
2020-03-29 01:05:44 +0800

    add: .env file to hide api keys

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,6 @@
-const User = require('../schemas/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const User = require("../schemas/user")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 exports.create = function(req, res) {
   console.log(req.body)
@@ -9,34 +9,46 @@ exports.create = function(req, res) {
     password: req.body.password,
     name: req.body.name
   }
-  User.create(user).then((User) => {
-    res.send({ success: true, result: [User], errors: [] })
-    console.log('saved to db')
-  }).catch((err) => {
-    res.send({ success: false, result: [], errors: [err] })
-    console.log(err)
-  })
+  User.create(user)
+    .then(User => {
+      res.send({ success: true, result: [User], errors: [] })
+      console.log("saved to db")
+    })
+    .catch(err => {
+      res.send({ success: false, result: [], errors: [err] })
+      console.log(err)
+    })
 }
 
 exports.readOne = function(req, res, next) {
-  if (req.body.email !== '' && req.body.password !== '') {
-    User.findOne({
-      email: req.body.email
-    }, function(err, userInfo) {
-      if (userInfo === null) {
-        // next(err);
-        res.send({ success: false, result: [], errors: [err] })
-      } else {
-        if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-          const token = jwt.sign({
-            id: userInfo._id,
-          }, req.app.get('secretKey'), { expiresIn: '24h' })
-          res.json({ status: 'success', message: 'User Found', data: { user: userInfo, token: token } })
-        } else {
+  if (req.body.email !== "" && req.body.password !== "") {
+    User.findOne(
+      {
+        email: req.body.email
+      },
+      function(err, userInfo) {
+        if (userInfo === null) {
           res.send({ success: false, result: [], errors: [err] })
+        } else {
+          if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+            const token = jwt.sign(
+              {
+                id: userInfo._id
+              },
+              req.app.get("secretKey"),
+              { expiresIn: "24h" }
+            )
+            res.json({
+              status: "success",
+              message: "User Found",
+              data: { user: userInfo, token: token }
+            })
+          } else {
+            res.send({ success: false, result: [], errors: [err] })
+          }
         }
       }
-    })
+    )
   } else {
     res.send({ success: false, result: [], errors: [err] })
   }
--- server/server.js
+++ server/server.js
@@ -4,20 +4,19 @@ const cors = require("cors")
 const port = 3000
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-let jwt = require("express-jwt")
+const jwt = require("express-jwt")
+const dotenv = require("dotenv")
 
-// let upload = multer({ dest: 'uploads/' })
+dotenv.config()
+
 app.use(express.static("uploads"))
 mongoose
-  .connect(
-    "mongodb+srv://dbadmin:fuckRaouf@cluster0-jyz9i.mongodb.net/helpDB?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
   .then(info => {
     console.log("mongo connected")
   })
@@ -25,25 +24,31 @@ mongoose
     console.log("mongo connection error", err.message)
   })
 
-app.set("secretKey", "server")
+app.set("secretKey", process.env.TOKEN_SECRET)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
-// app.use(jwt({
-//   secret: 'server',
-//   getToken: function fromHeaderOrQuerystring(req) {
-//     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-//       return req.headers.authorization.split(' ')[1];
-//     } else if (req.query && req.query.token) {
-//       return req.query.token;
-//     }
-//     return null;
-//   }
-// }).unless({path: ['/', '/user/auth', '/user/register']}), function (err, req, res, next) {
-//   if (err) {
-//     res.sendStatus(401);
-//   }
-// });
+app.use(
+  jwt({
+    secret: process.env.TOKEN_SECRET,
+    getToken: function fromHeaderOrQuerystring(req) {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+      ) {
+        return req.headers.authorization.split(" ")[1]
+      } else if (req.query && req.query.token) {
+        return req.query.token
+      }
+      return null
+    }
+  }).unless({ path: ["/user/auth", "/user/register"] }),
+  function(err, req, res, next) {
+    if (err) {
+      res.sendStatus(401)
+    }
+  }
+)
 app.get("/", (req, res) => res.send("Hello World!"))
 app.use("/user", require("./routes/user.route"))
 app.use("/journals", require("./routes/journal.route"))
