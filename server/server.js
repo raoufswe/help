e641b240ff77b74e26bf61808dfdb21d660773e9
raoufswe <abdulraoufswehli58@gmail.com>
2020-05-03 20:28:28 +0800

    refactor: dynamic port number

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const jwt = require('express-jwt')
